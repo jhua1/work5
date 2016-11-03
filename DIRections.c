d01f620d73fbd232829f841338c71de5f5b165d9
james.hua <jhua@stuy.edu>
2016-11-03 01:03:21 -0400

    finished

--- DIRections.c
+++ DIRections.c
@@ -2,20 +2,29 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/types.h>
-
-void print(DIR* strm){
-  struct dirent *yes;
-
-  while(yes = readdir(strm)){
-    printf("%s\n", yes->d_name);
-  }
-}
+#include <sys/stat.h>
 
 int main(){
   DIR* stream = opendir(".");
-  print(stream);
-
-  closedir(stream);
   
+  struct dirent *yes;
+
+  int size = 0;
+
+  while(yes = readdir(stream)){
+    if (yes->d_type == DT_DIR)
+      printf("%s (Directory)\n", yes->d_name);
+    
+    else
+      printf("%s\n", yes->d_name);
+   
+    struct stat filestat;
+    stat(yes->d_name,&filestat);
+    size+= filestat.st_size;    
+  }
+  
+  closedir(stream);
+
+  printf("Total size of directory: %d Bytes\n", size);
   return 0;
 }
